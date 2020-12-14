9ba7395bd86bb384d5798cf27251b3650007e23b
赵新乐 <tomcat@hisazo.group>
2020-12-14 23:25:58 +0800

    返回实体增加备注

--- src/main/java/com/xslgy/core/utils/Result.java
+++ src/main/java/com/xslgy/core/utils/Result.java
@@ -1,20 +1,26 @@
 package com.xslgy.core.utils;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel(value = "返回结果")
 public class Result<T> {
     /**
      * 错误码
      */
+    @ApiModelProperty(value = "状态码：200正常，非200为异常")
     private int code;
     /**
      * 消息
      */
+    @ApiModelProperty(value = "返回消息")
     private String message;
     /**
      * 数据
      */
+    @ApiModelProperty(value = "返回数据")
     private T data;
 
     public Result(int code, String message, T data) {
