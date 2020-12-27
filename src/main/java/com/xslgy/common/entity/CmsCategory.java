5f1ce7cd6926e0c84e3fb6fbe05a1d654f0895d0
赵新乐 <tomcat@hisazo.group>
2020-12-27 14:09:54 +0800

    分类增加排序

--- src/main/java/com/xslgy/common/entity/CmsCategory.java
+++ src/main/java/com/xslgy/common/entity/CmsCategory.java
@@ -11,9 +11,28 @@ import javax.persistence.Table;
 @Table(name = "cms_category")
 public class CmsCategory extends BaseEntity {
 
+    /**
+     * 编码
+     */
     private String code;
+    /**
+     * 分类名称
+     */
     private String name;
+    /**
+     * 父级id
+     */
     private Long parentId;
+    /**
+     * 图片地址
+     */
     private String imgUrl;
-    private String description;
+    /**
+     * 排序
+     */
+    private Integer orerNo;
+    /**
+     * 描述
+     */
+    private String remark;
 }
