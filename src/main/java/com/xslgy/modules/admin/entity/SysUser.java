368069998708949e05219a8d2b13728363cc061a
赵新乐 <tomcat@hisazo.group>
2021-01-09 22:18:14 +0800

    username和mobile增加唯一索引

--- src/main/java/com/xslgy/modules/admin/entity/SysUser.java
+++ src/main/java/com/xslgy/modules/admin/entity/SysUser.java
@@ -4,11 +4,13 @@ import com.xslgy.core.entity.BaseEntity;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.Table;
 
 @Data
 @Entity
-@Table(name = "sys_user")
+@Table(name = "sys_user", indexes = {@Index(name = "sys_user_usernmae_idx", columnList = "username", unique = true),
+                                    @Index(name = "sys_user_mobile_idx", columnList = "mobile", unique = true)})
 public class SysUser extends BaseEntity {
     private String username;
     private String password;
