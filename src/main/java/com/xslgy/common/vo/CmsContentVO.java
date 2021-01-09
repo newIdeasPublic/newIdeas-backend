fb65753f41ad8e26259644ca3650338243ae45c8
ivu4e <ivu4e@qq.com>
2021-01-09 14:40:11 +0800

    增加备注、点击量、作者等字段

--- src/main/java/com/xslgy/common/vo/CmsContentVO.java
+++ src/main/java/com/xslgy/common/vo/CmsContentVO.java
@@ -29,5 +29,14 @@ public class CmsContentVO {
     private Integer orderNo;
     @ApiModelProperty("状态")
     private Integer status;
-
+    @ApiModelProperty("备注")
+    @Column(name = "remark", length = 4096)
+    private String remark;
+    @ApiModelProperty("点击量")
+    private Integer clickCount;
+    @ApiModelProperty("作者")
+    private String author;
+    @ApiModelProperty("标签，以分号分隔")
+    @Column(name = "tags", length = 255)
+    private String tags;
 }
