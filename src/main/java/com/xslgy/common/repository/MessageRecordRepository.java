71263f56b90cf74054ea0a963837c96c404fad31
赵新乐 <tomcat@hisazo.group>
2021-02-23 11:14:43 +0800

    处理短信60秒限制的问题

--- src/main/java/com/xslgy/common/repository/MessageRecordRepository.java
+++ src/main/java/com/xslgy/common/repository/MessageRecordRepository.java
@@ -4,6 +4,10 @@ import com.xslgy.common.entity.MessageRecord;
 import com.xslgy.core.repository.BaseRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface MessageRecordRepository extends BaseRepository<MessageRecord, Long> {
+
+    MessageRecord getByTypeAndReceiverAndUpdateTimeAfter(Integer type, String receiver, Date time);
 }
--- src/main/java/com/xslgy/common/service/impl/MessageRecordServiceImpl.java
+++ src/main/java/com/xslgy/common/service/impl/MessageRecordServiceImpl.java
@@ -1,11 +1,15 @@
 package com.xslgy.common.service.impl;
 
+import com.xslgy.common.entity.MessageRecord;
 import com.xslgy.common.repository.MessageRecordRepository;
 import com.xslgy.common.service.MessageRecordService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Slf4j
 @Service
@@ -14,4 +18,14 @@ public class MessageRecordServiceImpl implements MessageRecordService {
     @Resource
     MessageRecordRepository messageRecordRepository;
 
+    @Override
+    public MessageRecord save(MessageRecord messageRecord) {
+        return messageRecordRepository.save(messageRecord);
+    }
+
+    @Override
+    public MessageRecord getByTypeAndReceiver(Integer type, String receiver) {
+        return messageRecordRepository.getByTypeAndReceiverAndUpdateTimeAfter(type, receiver,
+                Date.from(LocalDateTime.now().minusSeconds(60).atZone(ZoneId.systemDefault()).toInstant()));
+    }
 }
